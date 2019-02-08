0e142123102ae4436ff555bf0d1146de129f67c1
Abuhuraira Lakdawala <Abuhuraira.Lakdawala@vistaprint.com>
2019-02-08 12:58:33 +0530

    initial repo

---
+++ src/IndianShareMarket.php
@@ -0,0 +1,12 @@
+<?php
+namespace IndianShareMarket;
+
+use IndianShareMarket\Services\ParseDocument;
+
+class IndianShareMarket
+{
+    public function getQuote($code)
+    {
+        
+    }
+}
---
+++ src/Services/ParseDocument.php
@@ -0,0 +1,86 @@
+<?php
+namespace IndianShareMarket\Services;
+
+class ParseDocument
+{
+    private $domDocument;
+
+    /**
+     * ParseDocument constructor
+     */
+    public function __construct()
+    {
+        $this->setDomDocument();
+    }
+
+    /**
+     * Get domDocument object
+     * 
+     * @return domDocument
+     */
+    public function getDomDocument()
+    {
+        return $this->domDocument;
+    }
+
+    /**
+     * Set domDocument object
+     */
+    public function setDomDocument()
+    {
+        $this->domDocument = new \domDocument();
+    }
+
+    /**
+     * Initialize scrapping websites
+     * 
+     * @param  string $url
+     * @return bool
+     */
+    public function get($url)
+    {
+        $context = stream_context_create(
+            array(
+                'http' => array(
+                    'header' => array('User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201'),
+                    'timeout' => 10
+                ),
+            )
+        );
+
+        $file = @file_get_contents(
+            $url,
+            false,
+            $context
+        );
+        libxml_use_internal_errors(true);
+
+        return ($file) ? $this->domDocument->loadHTML($file) : false;
+    }
+
+    /**
+     * Search dom by id attribute
+     * 
+     * @param  string $id
+     * @return string
+     */
+    public function findId($id)
+    {
+        $element = $this->domDocument->getElementById($id);
+
+        return $element->nodeValue;
+    }
+
+    /**
+     * Search dom by class attribute
+     * 
+     * @param  string $class
+     * @return DOMNodeList
+     */
+    public function findClass($class)
+    {
+        $finder = new \DomXPath($this->domDocument);
+
+        return $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), '$class')]");
+    }
+}
